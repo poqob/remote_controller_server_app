9a2d505c09e0511ae47487b65f6a76cb2bb52ba7
Mustafa <mmermmus123@gmail.com>
2023-10-25 01:57:49 +0300

    dynamic model0(working)

--- src/model/Mouse/MouseController.py
+++ src/model/Mouse/MouseController.py
@@ -16,7 +16,7 @@ class MouseController:
         pass
 
     def handle(self, mouseModel: MouseModel):
-        print(mouseModel.mouseBehaviour)
+        # print(mouseModel.mouseBehaviour)
         self.currentAction = mouseModel
         if mouseModel.mouseBehaviour == MousePadBehaviour.STATIC:
             self._static_pad_controller()
@@ -67,19 +67,17 @@ class MouseController:
 
     dynamicPadCompanent: MouseModel = None
 
-    # TODO: dynamic move
-    # take packages to a structure
-    # p0, p1, p2
-    # then calculate difference between p2 and p1.
-    # then return an (x,y)
-    # add new (x,y) to mouse position.
-    # apply new possition to move~drag
-    # action2 is currentAction
-    # suppose have x,y,action space:(0,0)(0,1)(1,0)(1,1)||(0,0)(0,-1)(-1,0)(-1,-1)
     def _dynamic_pad_controller(self):
-        print("TEST")
-        ret, self.dynamicPadCompanent = self.packageHandler.manage(self.currentAction)
-        print(self.dynamicPadCompanent)
+        # print("TEST")
+        # ret, self.dynamicPadCompanent = self.packageHandler.manage(self.currentAction)
+        print(self.currentAction.x, self.currentAction.y)
+        ret = True
+        DRAG_TOLERANCE_VALUE = 20
+        if (
+            abs(self.currentAction.x) > DRAG_TOLERANCE_VALUE
+            or abs(self.currentAction.y) > DRAG_TOLERANCE_VALUE
+        ):
+            ret = False
         if ret is True:
             match self.currentAction.action:
                 case MouseActions.LEFT_CLICK:
@@ -97,32 +95,31 @@ class MouseController:
                 case MouseActions.MOVE:  ## dynamic move
                     x, y = mouse.get_position()
                     mouse.move(
-                        x + self.dynamicPadCompanent[0],
-                        y + self.dynamicPadCompanent[1],
+                        x + self.currentAction.x,
+                        y + self.currentAction.y,
                         absolute=True,
                         duration=0,
                     )
+                    # TODO: drag problem for dynamic pad.
                 case MouseActions.DRAG_START:  ## later updates
                     x, y = mouse.get_position()
                     # mouse.drag(start_x, start_y, end_x, end_y, absolute=True, duration=0)
                     if mouse.is_pressed(button="left") is False:
                         mouse.press(button="left")
                     mouse.move(
-                        x + self.dynamicPadCompanent[0],
-                        y + self.dynamicPadCompanent[1],
+                        x + self.currentAction.x,
+                        y + self.currentAction.y,
                         absolute=True,
                         duration=0,
                     )
                 case MouseActions.DRAG_CANCEL:
                     if mouse.is_pressed(button="left") is True:
                         mouse.release(button="left")
-                        self.packageHandler.dequeue()
 
                 case MouseActions.RELEASE:
                     if mouse.is_pressed(button="left") is True:
                         mouse.release(button="left")
                     if mouse.is_pressed(button="right") is True:
                         mouse.release(button="right")
-                    self.packageHandler.dequeue()
                 case _:
                     pass
