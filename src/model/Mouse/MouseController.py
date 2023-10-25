3b827c86a0a928cb8a35436dc6369757d5e0bc26
Mustafa <mmermmus123@gmail.com>
2023-10-25 12:12:07 +0300

    package works well dynamic pad design done.

--- src/model/Mouse/MouseController.py
+++ src/model/Mouse/MouseController.py
@@ -18,25 +18,14 @@ class MouseController:
     def handle(self, mouseModel: MouseModel):
         # print(mouseModel.mouseBehaviour)
         self.currentAction = mouseModel
-        if mouseModel.mouseBehaviour == MousePadBehaviour.STATIC:
-            self._static_pad_controller()
-        elif mouseModel.mouseBehaviour == MousePadBehaviour.DYNAMIC:
-            self._dynamic_pad_controller()
+        if self._common_events() is False:
+            if mouseModel.mouseBehaviour == MousePadBehaviour.STATIC:
+                self._static_pad_controller()
+            elif mouseModel.mouseBehaviour == MousePadBehaviour.DYNAMIC:
+                self._dynamic_pad_controller()
 
     def _static_pad_controller(self):
         match self.currentAction.action:
-            case MouseActions.LEFT_CLICK:
-                mouse.click(button="left")
-            case MouseActions.RIGHT_CLICK:
-                mouse.click(button="right")
-            case MouseActions.DOUBLE_CLICK_LEFT:
-                mouse.double_click(button="left")
-            case MouseActions.DOUBLE_CLICK_RIGHT:
-                mouse.double_click(button="right")
-            case MouseActions.SCROLL_DOWN:
-                mouse.wheel(delta=-SCROLL_POWER_VALUE)
-            case MouseActions.SCROLL_UP:
-                mouse.wheel(delta=SCROLL_POWER_VALUE)
             case MouseActions.MOVE:  ## static move
                 mouse.move(
                     self.currentAction.x,
@@ -65,50 +54,59 @@ class MouseController:
             case _:
                 pass
 
-    dynamicPadCompanent: MouseModel = None
+    def _common_events(self) -> bool:
+        match self.currentAction.action:
+            case MouseActions.LEFT_CLICK:
+                mouse.click(button="left")
+                return True
+            case MouseActions.RIGHT_CLICK:
+                mouse.click(button="right")
+                return True
+            case MouseActions.DOUBLE_CLICK_LEFT:
+                mouse.double_click(button="left")
+                return True
+            case MouseActions.DOUBLE_CLICK_RIGHT:
+                mouse.double_click(button="right")
+                return True
+            case MouseActions.SCROLL_DOWN:
+                mouse.wheel(delta=-SCROLL_POWER_VALUE)
+                return True
+            case MouseActions.SCROLL_UP:
+                mouse.wheel(delta=SCROLL_POWER_VALUE)
+                return True
+            case MouseActions.RELEASE:
+                if mouse.is_pressed(button="left") is True:
+                    mouse.release(button="left")
+                if mouse.is_pressed(button="right") is True:
+                    mouse.release(button="right")
+                return True
+            case _:
+                return False
 
     def _dynamic_pad_controller(self):
-        # print("TEST")
-        # ret, self.dynamicPadCompanent = self.packageHandler.manage(self.currentAction)
-        print(self.currentAction.x, self.currentAction.y)
-        ret = True
+        ret, res = self.packageHandler.manage(self.currentAction)
+        # print(res[0], res[1], ret)
         DRAG_TOLERANCE_VALUE = 20
-        if (
-            abs(self.currentAction.x) > DRAG_TOLERANCE_VALUE
-            or abs(self.currentAction.y) > DRAG_TOLERANCE_VALUE
-        ):
+        if abs(res[0]) > DRAG_TOLERANCE_VALUE or abs(res[1]) > DRAG_TOLERANCE_VALUE:
             ret = False
+
         if ret is True:
             match self.currentAction.action:
-                case MouseActions.LEFT_CLICK:
-                    mouse.click(button="left")
-                case MouseActions.RIGHT_CLICK:
-                    mouse.click(button="right")
-                case MouseActions.DOUBLE_CLICK_LEFT:
-                    mouse.double_click(button="left")
-                case MouseActions.DOUBLE_CLICK_RIGHT:
-                    mouse.double_click(button="right")
-                case MouseActions.SCROLL_DOWN:
-                    mouse.wheel(delta=-SCROLL_POWER_VALUE)
-                case MouseActions.SCROLL_UP:
-                    mouse.wheel(delta=SCROLL_POWER_VALUE)
                 case MouseActions.MOVE:  ## dynamic move
                     x, y = mouse.get_position()
                     mouse.move(
-                        x + self.currentAction.x,
-                        y + self.currentAction.y,
+                        x + res[0],
+                        y + res[1],
                         absolute=True,
                         duration=0,
                     )
-                    # TODO: drag problem for dynamic pad.
                 case MouseActions.DRAG_START:  ## later updates
                     x, y = mouse.get_position()
-                    # mouse.drag(start_x, start_y, end_x, end_y, absolute=True, duration=0)
                     if mouse.is_pressed(button="left") is False:
                         mouse.press(button="left")
                     mouse.move(
-                        x + self.currentAction.x,
-                        y + self.currentAction.y,
+                        x + res[0],
+                        y + res[1],
                         absolute=True,
                         duration=0,
                     )
@@ -116,10 +114,5 @@ class MouseController:
                     if mouse.is_pressed(button="left") is True:
                         mouse.release(button="left")
 
-                case MouseActions.RELEASE:
-                    if mouse.is_pressed(button="left") is True:
-                        mouse.release(button="left")
-                    if mouse.is_pressed(button="right") is True:
-                        mouse.release(button="right")
                 case _:
                     pass
