72859adf21d9932d47900daf042766fb3ebc00a7
Mustafa <mmermmus123@gmail.com>
2023-10-25 12:11:48 +0300

    package handler mechanism changed hope works well

--- src/model/Mouse/MousePackageHandler.py
+++ src/model/Mouse/MousePackageHandler.py
@@ -3,27 +3,22 @@ from queue import Queue
 
 
 class MousePackageHandler:
-    queue = Queue(maxsize=3)
     model0: MouseModel = None  # firstly called mouse action.
     model1: MouseModel = None  # lastly called mouse action.
+    res: tuple = (0, 0)
     isReadyToGetPackage: bool = False
 
     def __init__(self) -> None:
         pass
 
     def manage(self, model: MouseModel):
-        if self.queue.full():
-            self.model0 = self.queue.get()
-            self.model1 = self.queue.get()
-            res = self.model1.x - self.model0.x, self.model1.y - self.model0.y  # x, y
-            self.isReadyToGetPackage = True
-            if abs(res[0]) > 5 or abs(res[1]) > 5:
-                self.isReadyToGetPackage = False
-        else:
-            res = None
+        if self.model0 == None:
+            self.model0 = model
             self.isReadyToGetPackage = False
-        self.queue.put(model)
-        return self.isReadyToGetPackage, res
-
-    def dequeue(self):
-        self.queue = Queue(maxsize=3)
+        if self.model1 == None and self.model0 != None:
+            self.model1 = model
+            self.res = (self.model1.x - self.model0.x, self.model1.y - self.model0.y)
+            self.isReadyToGetPackage = True
+            self.model0 = self.model1
+            self.model1 = None
+            return self.isReadyToGetPackage, self.res
